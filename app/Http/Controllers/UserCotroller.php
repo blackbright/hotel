fd80f1e6da6f4cfa15ef9c1898cb7cc1ce5c21e7
blackbright <blackbright@gmail.com>
2024-03-08 15:45:52 +0700

    init frontend

---
+++ app/Http/Controllers/UserCotroller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserCotroller extends Controller
+{
+    public function Index()
+    {
+        return view('frontend.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserCotroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [UserCotroller::class, 'Index']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
